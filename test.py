a020a84df9b205150be997ede9ab8020c9f8e4b5
s174831 <s174831@student.dtu.dk>
2021-09-28 14:11:05 +0200

    include csv file

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-#import pandas as pd
+import pandas as pd
 
-#mydata = pd.read_csv("http://www-stat.stanford.edu/~tibs/ElemStatLearn/datasets/SAheart.data")
-#print(mydata)
-
-def f(x):
-    def g(y):
-        x+y
-    return g
-
-a = f(5)
-b = a(5)
-print(b)
-
+mydata = pd.read_csv("african-data.csv")
+print(mydata)
 
